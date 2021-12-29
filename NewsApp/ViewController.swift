b015b5589ddafbd2600bfb7e7351ef83e1443e84
Youssef <youssef.b.air2@gmail.com>
2021-12-29 22:44:18 +0100

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Youssef Bhl on 29/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
